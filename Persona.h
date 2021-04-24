b42f119194da7bdf544eb966191b9da0369f985f
Samuel Ortiz <samuel.om@alliedglobalbpo.com>
2021-04-24 08:58:08 -0600

    Agregar archivos de proyecto.

---
+++ Persona.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <iostream>
+using namespace std;
+
+class Persona
+{
+	protected: string nombres, apellidos, direccion;
+			 int telefono;
+			 //constructor
+	protected:
+		Persona() {
+		}
+		Persona(string nom, string ape, string dir, int tel) {
+			nombres = nom;
+			apellidos = ape;
+			direccion = dir;
+			telefono = tel;
+		}
+		void mostrar();
+};
+
---
+++ poo_c_mysql.cpp
@@ -0,0 +1,11 @@
+// poo_c_mysql.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+}
+
